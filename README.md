ee1848e8f6515ad4a27d16045678ab8d37672adb
muriithi <josephkagotho04@gmail.com>
2025-01-08 16:33:58 +0300

    Update app.py, requirements.txt, README.md, maps, and datasets

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# tec_app_kenya
+# ***Near Real-Time Total Electron Content (TEC) Over Kenya***
+
+This Streamlit app visualizes hourly Total Electron Content (TEC) over Kenya and automatically updates every 5 minutes. TEC is an important parameter affecting the accuracy of Global Navigation Satellite Systems (GNSS), including GPS. This app downloads Near-Real TEC data, processes it, trains a machine learning model to predict TEC values, and displays the results on a map.
+
+## ***Features***
+
+- **Automatic Data Download:** Downloads TEC data from the specified URLs.
+- **Data Processing:** Removes outliers and prepares data for training.
+- **Model Training:** Trains a TensorFlow model to predict TEC values.
+- **Visualization:** Plots the TEC values on a map, showing the predicted TEC over Kenya.
+- **Automatic Refresh:** Updates the data and visualization every 5 minutes.
+
+## ***Installation***
+
+### **Running the App Locally**
+
+To run the app locally, follow these steps:
+
+1. **Clone the repository:**
+    ```sh
+    git clone https://github.com/ngoosojk/tec_app_kenya.git
+    cd tec_app_kenya
+    ```
+
+2. **Install the required dependencies:**
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+3. **Run the Streamlit app:**
+    ```sh
+    streamlit run app.py
+    ```
+
+## ***Usage***
+
+1. **Run the App:**
+    - Start the app using the command `streamlit run app.py`. The app will open in your default web browser.
+
+2. **Automatic Updates:**
+    - The app automatically downloads new data every 5 minutes, processes it, trains the model, and updates the visualization.
+
+3. **TEC Visualization:**
+    - The map displays the predicted TEC values over Kenya. The color gradient represents the TEC values in TECU (Total Electron Content Units) and the associated Ionospheric range error in meters.
+
+4. **Reboot Interval:**
+    - The app is set to reboot every hour to ensure efficient performance. The next reboot time is displayed on the app.
+
+## ***Directory Structure***
+
+- `datasets/`: Directory to store downloaded data in JSON format.
+- `maps/`: Directory to save generated maps.
+- `app.py`: Main application script.
+- `requirements.txt`: File containing all the dependencies required to run the app.
+- `model.keras`: Zip file containing the model.
+
+## ***Configuration***
+
+- **Data Source URLs:**
+    - The app downloads data from the Malindi and Nairobi GNSS receivers, accessible on the ESWUA website (http://www.eswua.ingv.it/index.php/gnss-scintillation/gnss-real-time-data), using specified URLs.
+
+## ***Memory Management***
+
+- The app monitors memory usage and provides a warning if high memory usage is detected. This helps in optimizing the app for better performance.
+
+## **Contributing**
+
+Contributions are welcome! If you have any suggestions or improvements, please create a pull request or open an issue.
+
+## **Acknowledgements**
+
+- This app uses data from the Nairobi and Malindi GNSS receivers, owned by the Kenya Space Agency and the Italian Space Agency respectively, accessible on the ESWUA website.
+- Special thanks to the Streamlit community for their support and resources.
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import json
 import urllib.request
-import time
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
@@ -9,7 +8,6 @@ import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import EarlyStopping
 import psutil
 import pytz
@@ -20,17 +18,22 @@ os.makedirs('maps', exist_ok=True)
 
 # Function to download data from a given URL and return the filename
 def download_data(url, filename):
-    webURL = urllib.request.urlopen(url)
-    param = json.loads(webURL.read().decode())
-    parameters = param["records"]
+    try:
+        webURL = urllib.request.urlopen(url)
+        param = json.loads(webURL.read().decode())
+        parameters = param["records"]
 
-    # Create a JSON file with the data in the datasets folder
-    filepath = os.path.join('datasets', filename)
-    with open(filepath, 'w') as sample:
-        json.dump(parameters, sample, indent=4, separators=(',', ': '))
+        # Create a JSON file with the data in the datasets folder
+        filepath = os.path.join('datasets', filename)
+        with open(filepath, 'w') as sample:
+            json.dump(parameters, sample, indent=4, separators=(',', ': '))
 
-    print(f"Data downloaded and saved to {filepath}")
-    return filepath
+        print(f"Data downloaded and saved to {filepath}")
+        return filepath
+
+    except Exception as e:
+        print(f"Failed to download data from {url}. The servers cannot be reached. Error: {e}")
+        return None
 
 # Function to remove outliers using the IQR method and a minimum threshold for vtec values
 def remove_outliers(ipp_lons, ipp_lats, vtec_values, min_vtec_threshold=1.0):
@@ -38,6 +41,10 @@ def remove_outliers(ipp_lons, ipp_lats, vtec_values, min_vtec_threshold=1.0):
     filtered_ipp_lons = [ipp_lons[i] for i in valid_indices]
     filtered_ipp_lats = [ipp_lats[i] for i in valid_indices]
     filtered_vtec_values = [vtec_values[i] for i in valid_indices]
+
+    if len(filtered_vtec_values) == 0:
+        print("No valid VTEC values found after filtering.")
+        return np.array([]), np.array([]), np.array([])
 
     Q1 = np.percentile(filtered_vtec_values, 25)
     Q3 = np.percentile(filtered_vtec_values, 75)
@@ -57,7 +64,7 @@ def prepare_data(ipp_lons, ipp_lats, vtec_values):
     X = np.column_stack((ipp_lons, ipp_lats))
     y = np.array(vtec_values)
     return X, y
-    
+
 # Function to train the model with early stopping
 def train_model(X_train, y_train, model_path='model.keras'):
     model = tf.keras.models.Sequential([
@@ -84,7 +91,6 @@ def train_model(X_train, y_train, model_path='model.keras'):
     return model
 
 # Function to predict VTEC values using the trained model
-
 def predict_vtec(model, lons, lats):
     X_pred = np.column_stack((lons, lats))
     vtec_pred = model.predict(X_pred)
@@ -96,6 +102,10 @@ def plot_data(filenames, model_path='model.keras'):
     vtec_values = []
 
     for filename in filenames:
+        if filename is None:
+            print("Skipping plot due to failed data download.")
+            return
+
         with open(filename, 'r') as f:
             data = json.load(f)
         ipp_lons.extend([record['ipp_lon'] for record in data if record['elevation'] is not None and record['elevation'] >= 30])
@@ -103,15 +113,15 @@ def plot_data(filenames, model_path='model.keras'):
         vtec_values.extend([record['vtec'] for record in data if record['elevation'] is not None and record['elevation'] >= 30])
 
     filtered_ipp_lons, filtered_ipp_lats, filtered_vtec_values = remove_outliers(ipp_lons, ipp_lats, vtec_values)
+
+    if len(filtered_vtec_values) == 0:
+        print("No valid data to plot after removing outliers.")
+        return
+
     X_train, y_train = prepare_data(filtered_ipp_lons, filtered_ipp_lats, filtered_vtec_values)
-    
-    # Check if the model file exists before loading or training
-    if os.path.exists(model_path):
-        model = load_model(model_path, compile=False)
-        model.compile(optimizer='adam', loss='mean_absolute_error')
-        print("Loaded existing model.")
-    else:
-        model = train_model(X_train, y_train, model_path)
+
+    # Train a new model each time
+    model = train_model(X_train, y_train, model_path)
 
     min_latitude = -5.0
     max_latitude = 5.2
@@ -119,11 +129,9 @@ def plot_data(filenames, model_path='model.keras'):
     max_longitude = 42.0
 
     grid_lons, grid_lats = np.meshgrid(np.linspace(min_longitude, max_longitude, 100), np.linspace(min_latitude, max_latitude, 100))
-    grid_lons = grid_lons.flatten()
-    grid_lats = grid_lats.flatten()
-    grid_vtec = predict_vtec(model, grid_lons, grid_lats)
+    grid_vtec = predict_vtec(model, grid_lons.flatten(), grid_lats.flatten())
 
-    fig, ax = plt.subplots(figsize=(10, 5))
+    fig, ax = plt.subplots(figsize=(8, 7))  # Increase the size of the map
     m = Basemap(projection='merc', llcrnrlat=min_latitude, urcrnrlat=max_latitude,
                 llcrnrlon=min_longitude, urcrnrlon=max_longitude,
                 resolution='l', ax=ax)
@@ -144,7 +152,7 @@ def plot_data(filenames, model_path='model.keras'):
         else:
             direction = '°N' if lat > 0 else '°S'
             label = f'{abs(lat)}{direction}'
-        plt.text(x, y, label, ha='right', va='center', fontsize=8, color='black')
+        plt.text(x, y, label, ha='right', va='center', fontsize=11, color='black')
 
     # Add longitude labels with °E, °W, or 0°
     for lon in meridians:
@@ -154,25 +162,43 @@ def plot_data(filenames, model_path='model.keras'):
         else:
             direction = '°E' if lon > 0 else '°W'
             label = f'{abs(lon)}{direction}'
-        plt.text(x, y, label, ha='center', va='top', fontsize=8, color='black')
+        plt.text(x, y, label, ha='center', va='top', fontsize=11, color='black')
 
-    # Plot the data
     x, y = m(grid_lons, grid_lats)
+    x = x.reshape(grid_lons.shape)
+    y = y.reshape(grid_lats.shape)
     sc = m.scatter(x, y, c=grid_vtec.flatten(), cmap='viridis', marker='o', vmin=0, vmax=100)
 
-    plt.colorbar(sc, label='TECU')
-    
+    # Add dotted contour lines at whole number intervals
+    contour_levels = np.arange(np.floor(grid_vtec.min()), np.ceil(grid_vtec.max()) + 1, 5)
+    cs = m.contour(x, y, grid_vtec.reshape(grid_lons.shape), levels=contour_levels, colors='white', linewidths=1.5, linestyles='dotted')
+    plt.clabel(cs, inline=1, fontsize=8, fmt='%1.0f')
+
+    # Add the main colorbar for TECU
+    cbar = plt.colorbar(sc, ax=ax, fraction=0.036, pad=0.1)
+    cbar.set_label('TECU', labelpad=-50, y=0.5, rotation=90, ha='center')
+
+    # Calculate ionospheric range error (L1) values
+    ionospheric_error = (3.24 / 20) * grid_vtec.flatten()
+
+    # Create a second colorbar axis for Ionospheric range error (L1)/m
+    cbar2_ax = cbar.ax.twinx()  # Create a twin of the main colorbar axis
+    cbar2_ax.set_ylim(sc.get_clim())  # Set the limits of cbar2 to match the main colorbar
+    cbar2_ax.set_yticks(cbar.get_ticks())  # Set the ticks of cbar2 to match the main colorbar
+    cbar2_ax.set_yticklabels([(3.24 / 20) * t for t in cbar.get_ticks()])  # Set the tick labels for cbar2
+    cbar2_ax.set_ylabel('Ionospheric range error (L1) in m', fontsize=12)  # Set the label for cbar2
+
     start_time_str = filenames[0].split('_')[1] + ' ' + filenames[0].split('_')[2] + ':' + filenames[0].split('_')[3] + ':' + filenames[0].split('_')[4]
     end_time_str = filenames[0].split('_')[5] + ' ' + filenames[0].split('_')[6] + ':' + filenames[0].split('_')[7] + ':' + filenames[0].split('_')[8].split('.')[0]
-    plt.title(f'Near Real-Time Hourly TEC Over Kenya\n{start_time_str} to {end_time_str} UTC')
+    plt.suptitle(f'Near Real-Time Hourly Total Electron Content (TEC) Over Kenya\n{start_time_str} to {end_time_str} UTC', fontsize=15)
 
     # Save the map as an image file in the maps folder
     map_filename = os.path.join('maps', f"map_{start_time_str.replace(' ', '_').replace(':', '-')}_to_{end_time_str.replace(' ', '_').replace(':', '-')}.png")
-    plt.savefig(map_filename)
+    plt.savefig(map_filename, bbox_inches='tight')  # Use bbox_inches='tight' to ensure proper alignment of ticks
 
-    plt.tight_layout()
+    plt.tight_layout()  # Adjust subplots to fit into figure area, leaving space for the title.
     st.pyplot(fig)
-    
+
 # Function to automate the process every 5 minutes
 @st.cache_data(ttl=240)
 def schedule_download_and_plot():
@@ -180,38 +206,19 @@ def schedule_download_and_plot():
     start_time = end_time - timedelta(hours=1)
     st_time = start_time.strftime("%Y-%m-%d%%20%H:%M:%S")
     en_time = end_time.strftime("%Y-%m-%d_%H:%M:%S")
-
+    
     nai_url = f'http://ws-eswua.rm.ingv.it/scintillation.php/records/wsnai0p?filter=dt,bt,{st_time},{en_time}&filter0=PRN,sw,&filter1=PRN,sw,N&filter2=PRN,sw,N&filter3=PRN,sw,N&filter4=PRN,sw,N&filter5=PRN,sw,N&filter6=PRN,sw,N&include=dt,PRN,vtec,ipp_lon,ipp_lat,elevation,&order=dt'
-    kil_url = f'http://ws-eswua.rm.ingv.it/scintillation.php/records/wsmal0p?filter=dt,bt,{st_time},{en_time}&filter0=PRN,sw,&filter1=PRN,sw,N&filter2=PRN,sw,N&filter3=PRN,sw,N&filter4=PRN,sw,N&filter5=PRN,sw,N&filter6=PRN,sw,N&include=dt,PRN,vtec,ipp_lon,ipp_lat,elevation,&order=dt'
+    mal_url = f'http://ws-eswua.rm.ingv.it/scintillation.php/records/wsmal0p?filter=dt,bt,{st_time},{en_time}&filter0=PRN,sw,&filter1=PRN,sw,N&filter2=PRN,sw,N&filter3=PRN,sw,N&filter4=PRN,sw,N&filter5=PRN,sw,N&filter6=PRN,sw,N&include=dt,PRN,vtec,ipp_lon,ipp_lat,elevation,&order=dt'
 
-    nai_filename = f'nai0p_{start_time.strftime("%Y-%m-%d_%H_%M_%S")}_{end_time.strftime("%Y-%m-%d_%H_%M_%S")}.json'
-    kil_filename = f'kil0p_{start_time.strftime("%Y-%m-%d_%H_%M_%S")}_{end_time.strftime("%Y-%m-%d_%H_%M_%S")}.json'
+    nai_filename = download_data(nai_url, f'nai0p_{start_time.strftime("%Y-%m-%d_%H_%M_%S")}_{end_time.strftime("%Y-%m-%d_%H_%M_%S")}.json')
+    mal_filename = download_data(mal_url, f'mal0p_{start_time.strftime("%Y-%m-%d_%H_%M_%S")}_{end_time.strftime("%Y-%m-%d_%H_%M_%S")}.json')
 
-    nai_filepath = download_data(nai_url, nai_filename)
-    kil_filepath = download_data(kil_url, kil_filename)
-
-    # Prepare data and train the model
-    ipp_lons = []
-    ipp_lats = []
-    vtec_values = []
-
-    for filepath in [nai_filepath, kil_filepath]:
-        with open(filepath, 'r') as f:
-            data = json.load(f)
-        ipp_lons.extend([record['ipp_lon'] for record in data if record['elevation'] is not None and record['elevation'] >= 30])
-        ipp_lats.extend([record['ipp_lat'] for record in data if record['elevation'] is not None and record['elevation'] >= 30])
-        vtec_values.extend([record['vtec'] for record in data if record['elevation'] is not None and record['elevation'] >= 30])
-
-    filtered_ipp_lons, filtered_ipp_lats, filtered_vtec_values = remove_outliers(ipp_lons, ipp_lats, vtec_values)
-    X_train, y_train = prepare_data(filtered_ipp_lons, filtered_ipp_lats, filtered_vtec_values)
-    model = train_model(X_train, y_train)
-
-    plot_data([nai_filepath, kil_filepath], model_path='model.keras')
+    plot_data([nai_filename, mal_filename], model_path='model.keras')
 
 # Streamlit app layout and execution
 st.title("Near Real-Time Total Electron Content (TEC) Over Kenya")
-st.write("This app visualizes hourly TEC over Kenya and automatically updates every 5 minutes. Note: 1 TECU (Total Electron Content Unit) is equal to 10^16 electrons/m² and can introduce an error of about 0.16 meters in Global Navigation Satellite Systems (GNSS) positioning, including in the Global Positioning System (GPS).")
-st_autorefresh(interval=60000, key="data_refresh")
+st.write("This app visualizes hourly TEC over Kenya and automatically updates every 5 minutes.")
+st_autorefresh(interval=300000, key="data_refresh")
 schedule_download_and_plot()
 
 # Set the interval (1 hour)
@@ -222,7 +229,7 @@ if "next_reboot_time" not in st.session_state:
     st.session_state.next_reboot_time = datetime.now() + timedelta(seconds=REBOOT_INTERVAL)
 
 # Display a message
-st.write(f"The app is set to reboot every {REBOOT_INTERVAL // 60} minutes ( 1 Hr) to ensure efficient performance.")
+st.write(f"This app visualizes hourly TEC over Kenya and automatically refreshes every 5 minutes. The app is set to reboot every {REBOOT_INTERVAL // 60} minutes ( 1 Hr) to ensure efficient performance.")
 
 # Check if it's time to reboot
 if datetime.now() >= st.session_state.next_reboot_time:
